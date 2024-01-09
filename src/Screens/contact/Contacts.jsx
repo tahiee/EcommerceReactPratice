9c6ffda8d4765ac33ed346b327b591e80204c236
tahiee <niazi.tahirkhan15@gmail.com>
2024-01-09 20:45:13 +0500

    style added

--- src/Screens/contact/Contacts.jsx
+++ src/Screens/contact/Contacts.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import GitImg from './github png.png'
 const Contacts = () => {
     return (
-        <div className='flex justify-center items-center text-center mt-10'>
-            <a href="https://github.com/tahiee">
+        <div className='flex justify-center items-center text-center'>
+        <div className='flex rounded-full h-[75px] w-[75px] justify-center items-center text-center mt-10 bg-white '>
+            <a className='' href="https://github.com/tahiee">
             <img className=' cursor-pointer h-[62px] w-[62px]' src={GitImg} alt="myGit" />
-
             </a>
+        </div>
         </div>
     )
 }
